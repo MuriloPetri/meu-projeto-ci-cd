223a005f6568b7afff4106f0dbd72201747dd24d
Murilo Petri <110271354+MuriloPetri@users.noreply.github.com>
2025-09-24 20:20:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-
 app = FastAPI()
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    # Testando o novo token de acesso
+    return {"message": "Hello World com CI/CD!"}
